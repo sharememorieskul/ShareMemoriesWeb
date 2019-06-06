daeb90f5d4f2d5d627ee62adb298cfce1bc36636
Michał Korniak <michalkorniak1@gmail.com>
2019-06-06 20:28:30 +0100

    Poprawka pobierania postów

--- src/app/shared/resolvers/get-articles-resolver.service.ts
+++ src/app/shared/resolvers/get-articles-resolver.service.ts
@@ -5,18 +5,35 @@ import { catchError } from 'rxjs/operators';
 
 import { Article } from '../models/article.model';
 import { ArticleService } from '../services/article.service';
+import { LocalStorageHelper } from '../helpers/local-storage.helper';
+import { Constants } from '../constants/constants';
+import { HttpHeaders } from '@angular/common/http';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class GetArticlesResolverService implements Resolve<Article[] | string> {
-  constructor(private articleService: ArticleService) {}
+  constructor(private articleService: ArticleService, private userService: UserService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Article[] | string> {
-    return this.articleService.getAll()
-      .pipe(
-        catchError((err: string) => of(err))
-      );
+    const userToken: string = LocalStorageHelper.getItem(Constants.localStorageTokenKey);
+    if (userToken == null) {
+      return this.articleService.getAll()
+        .pipe(
+          catchError((err: string) => of(err))
+        );
+    }
+    else {
+      return this.userService.GetAllPostsAvaiableForUser()
+        .pipe(
+          catchError((err: string) => of(err))
+        );
+    }
+
+
   }
+
+
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { Article } from '../models/article.model';
 import { catchError, map } from 'rxjs/operators';
+import { LocalStorageHelper } from '../helpers/local-storage.helper';
+import { Constants } from '../constants/constants';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ import { catchError, map } from 'rxjs/operators';
 export class UserService {
 
   private readonly apiPathAllUserArticles = 'https://afternoon-refuge-61557.herokuapp.com/users/df1dec3d-a1ea-49e5-9bcb-ea304a5750d5/posts';
-
+  private readonly apiPath = 'https://afternoon-refuge-61557.herokuapp.com/users/';
   constructor(private http: HttpClient) { }
 
   handleError(errorResponse: HttpErrorResponse) {
@@ -31,5 +33,23 @@ export class UserService {
         }),
         catchError(this.handleError)
       );
+  }
+  GetAllPostsAvaiableForUser(): Observable<Article[]> {
+    return this.http.get<Article[]>(this.apiPath + "posts",this.getOptionsRequest())
+      .pipe(
+        map((data: Article[]) => {
+          return data;
+        }),
+        catchError(this.handleError)
+      );
+  }
+  getOptionsRequest() {
+    const tokenObject = LocalStorageHelper.getItem(Constants.localStorageTokenKey);
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    if (tokenObject != null) {
+      headers = headers.append('Authorization', tokenObject.token);
+    }
+    return { headers: headers };
   }
 }
